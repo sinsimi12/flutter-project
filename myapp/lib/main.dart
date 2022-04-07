f8e7d2236241b588219f2cdde3886919c1adbe6a
Christian Mina <christian.mina@tup.edu.ph>
2022-04-08 00:52:42 +0800

    Add title text.

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('SariShop'),
+          title: const Text('SariShop'),
           centerTitle: true,
         ),
       ),
