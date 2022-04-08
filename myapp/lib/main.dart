c14ea42b30e6faf17b31ae3708984d5190fa02f9
Christian Mina <christian.mina@tup.edu.ph>
2022-04-08 22:14:22 +0800

    Add stateless class.

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -26,3 +26,10 @@ void main() => runApp(MaterialApp(
         ),
       ),
     ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
