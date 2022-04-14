faaab8ab47f313f89b6ea643751e71b22fab0a69
Christian Mina <christian.mina@tup.edu.ph>
2022-04-14 19:33:58 +0800

    Add home widget class component.

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: Scaffold(
+      home: Home(),
         appBar: AppBar(
           title: const Text('SariShop'),
           centerTitle: true,
