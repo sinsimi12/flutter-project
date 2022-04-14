6e1a191c0d3b1cb59cdcf6d5946dfd2686ceee97
Christian Mina <christian.mina@tup.edu.ph>
2022-04-14 19:34:24 +0800

    Add home stateless class widget.

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -2,6 +2,12 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
       home: Home(),
+    ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(
           title: const Text('SariShop'),
           centerTitle: true,
