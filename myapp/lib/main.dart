b1fcf52d3558a05e7692d1b3fd051d2c9d7c3873
Christian Mina <christian.mina@tup.edu.ph>
2022-04-14 19:34:49 +0800

    Add home properties widget.

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -8,34 +8,28 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('SariShop'),
-          centerTitle: true,
-          backgroundColor: Colors.green[600],
-        ),
-        body: const Center(
-          child: Text(
-            'Press 1 to Buy',
-            style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              color: Colors.grey,
-              fontFamily: 'Roboto',
-            ),
+      appBar: AppBar(
+        title: const Text('SariShop'),
+        centerTitle: true,
+        backgroundColor: Colors.green[600],
+      ),
+      body: const Center(
+        child: Text(
+          'Press 1 to Buy',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey,
+            fontFamily: 'Roboto',
           ),
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: const Text('Click!'),
-          backgroundColor: Colors.green,
-        ),
       ),
-    ));
-
-class Home extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Text('Click!'),
+        backgroundColor: Colors.green,
+      ),
+    );
   }
 }
